54ef5cf25b653702283d3e420208eb5f7c571a1d
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2021-04-19 00:34:10 +0200

    Add test for classNode.copy

---
+++ src/test/java/org/quiltmc/asmr/processor/AsmrClassNodeCopyTest.java
@@ -0,0 +1,34 @@
+package org.quiltmc.asmr.processor;
+
+import org.junit.jupiter.api.Test;
+import org.quiltmc.asmr.processor.tree.AsmrClassListNode;
+import org.quiltmc.asmr.processor.tree.AsmrClassNode;
+import org.quiltmc.asmr.processor.tree.AsmrTreeUtil;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.quiltmc.asmr.processor.AsmrClassTestUtil.findClass;
+
+public class AsmrClassNodeCopyTest {
+	@interface Foo {
+		String value();
+
+		String[] array() default {};
+	}
+
+	@Foo(value = "bar", array = {"foo", "baz"})
+	void method() {
+	}
+
+	@Test
+	public void testClassReader() {
+		AsmrClassNode classNode = findClass(AsmrClassNodeCopyTest.class);
+		String originalDump = AsmrTreeUtil.dump(classNode);
+
+		AsmrClassNode copy = classNode.copy(new AsmrClassListNode());
+		String copyDump = AsmrTreeUtil.dump(copy);
+
+		System.out.println(originalDump);
+
+		assertEquals(originalDump, copyDump);
+	}
+}
