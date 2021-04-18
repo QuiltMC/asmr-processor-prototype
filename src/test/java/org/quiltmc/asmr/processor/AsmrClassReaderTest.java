34394f9ee2c0443f5214cd78fc9dc0f12aeda349
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2021-04-19 00:28:33 +0200

    Extract findClass to util class

--- src/test/java/org/quiltmc/asmr/processor/AsmrClassReaderTest.java
+++ src/test/java/org/quiltmc/asmr/processor/AsmrClassReaderTest.java
@@ -1,21 +1,11 @@
 package org.quiltmc.asmr.processor;
 
 import org.junit.jupiter.api.Test;
-import org.objectweb.asm.ClassReader;
-import org.quiltmc.asmr.processor.tree.AsmrClassListNode;
 import org.quiltmc.asmr.processor.tree.AsmrClassNode;
 import org.quiltmc.asmr.processor.tree.AsmrTreeUtil;
-import org.quiltmc.asmr.processor.tree.asmvisitor.AsmrClassVisitor;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.quiltmc.asmr.processor.AsmrClassTestUtil.findClass;
 
 public class AsmrClassReaderTest {
     @interface Foo {
@@ -31,40 +21,5 @@ public class AsmrClassReaderTest {
         AsmrClassNode classNode = findClass(AsmrClassReaderTest.class);
         System.out.println(AsmrTreeUtil.dump(classNode));
         assertEquals(AsmrClassReaderTest.class.getName().replace('.', '/'), classNode.name().value());
-    }
-
-    private AsmrClassNode findClass(Class<?> clazz) {
-        File sourceLocation;
-        try {
-            sourceLocation = new File(clazz.getProtectionDomain().getCodeSource().getLocation().toURI());
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            throw new NoClassDefFoundError(clazz.getName());
-        }
-
-        ClassReader classReader;
-        try {
-            if (sourceLocation.isDirectory()) {
-                File classFile = new File(sourceLocation, clazz.getName().replace('.', File.separatorChar) + ".class");
-                try (InputStream in = new FileInputStream(classFile)) {
-                    classReader = new ClassReader(in);
-                }
-            } else {
-                try (JarFile jar = new JarFile(sourceLocation)) {
-                    JarEntry jarEntry = jar.getJarEntry(clazz.getName().replace('.', '/') + ".class");
-                    if (jarEntry == null) {
-                        throw new NoClassDefFoundError(clazz.getName());
-                    }
-                    classReader = new ClassReader(jar.getInputStream(jarEntry));
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new NoClassDefFoundError(clazz.getName());
-        }
-
-        AsmrClassNode classNode = new AsmrClassListNode().add();
-        classReader.accept(new AsmrClassVisitor(classNode), ClassReader.SKIP_FRAMES);
-        return classNode;
     }
 }
---
+++ src/test/java/org/quiltmc/asmr/processor/AsmrClassTestUtil.java
@@ -0,0 +1,54 @@
+package org.quiltmc.asmr.processor;
+
+import org.objectweb.asm.ClassReader;
+import org.quiltmc.asmr.processor.tree.AsmrClassListNode;
+import org.quiltmc.asmr.processor.tree.AsmrClassNode;
+import org.quiltmc.asmr.processor.tree.asmvisitor.AsmrClassVisitor;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+public class AsmrClassTestUtil {
+	private AsmrClassTestUtil() {
+	}
+
+	static AsmrClassNode findClass(Class<?> clazz) {
+		File sourceLocation;
+		try {
+			sourceLocation = new File(clazz.getProtectionDomain().getCodeSource().getLocation().toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+			throw new NoClassDefFoundError(clazz.getName());
+		}
+
+		ClassReader classReader;
+		try {
+			if (sourceLocation.isDirectory()) {
+				File classFile = new File(sourceLocation, clazz.getName().replace('.', File.separatorChar) + ".class");
+				try (InputStream in = new FileInputStream(classFile)) {
+					classReader = new ClassReader(in);
+				}
+			} else {
+				try (JarFile jar = new JarFile(sourceLocation)) {
+					JarEntry jarEntry = jar.getJarEntry(clazz.getName().replace('.', '/') + ".class");
+					if (jarEntry == null) {
+						throw new NoClassDefFoundError(clazz.getName());
+					}
+					classReader = new ClassReader(jar.getInputStream(jarEntry));
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new NoClassDefFoundError(clazz.getName());
+		}
+
+		AsmrClassNode classNode = new AsmrClassListNode().add();
+		classReader.accept(new AsmrClassVisitor(classNode), ClassReader.SKIP_FRAMES);
+		return classNode;
+	}
+}
