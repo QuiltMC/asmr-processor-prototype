f097df8ccb317fc4d7a50d393aefdd2ff0fe6b5d
williambl <william@williambl.com>
2021-05-10 18:19:09 +0100

    Revert AllowLambdaCapture back to using class retention

--- annotations/src/main/java/org/quiltmc/asmr/processor/annotation/AllowLambdaCapture.java
+++ annotations/src/main/java/org/quiltmc/asmr/processor/annotation/AllowLambdaCapture.java
@@ -6,6 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RetentionPolicy.CLASS)
 public @interface AllowLambdaCapture {
 }
